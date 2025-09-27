d53afb9fd69603191af71402e793bad153b52712
rashmiwijemanna <rashmiwijemanna7@gmail.com>
2025-09-27 12:20:16 +0530

    add OrderDetailRepository & update the getOrderDetails method usig layered architecture.

--- src/main/java/repository/OrderDetailRepository.java
+++ src/main/java/repository/OrderDetailRepository.java
@@ -1,7 +1,23 @@
 package repository;
 
+import db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class OrderDetailRepository {
-    public ResultSet get
+    public ResultSet getAllOrderDetails(){
+        Connection connection= null;
+        try {
+            connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM OrderDetail;");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- src/main/java/service/OrderDetailManagementControllerImpl.java
+++ src/main/java/service/OrderDetailManagementControllerImpl.java
@@ -4,6 +4,7 @@ import db.DBConnection;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import model.OrderDetailManagementDetails;
+import repository.OrderDetailRepository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,19 +12,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class OrderDetailManagementControllerImpl implements OrderDetailManagementService {
+    OrderDetailRepository  orderDetailRepository= new OrderDetailRepository();
     @Override
     public ObservableList<OrderDetailManagementDetails> getAllOrderDetails() {
         ObservableList<OrderDetailManagementDetails>orderDetailManagementDetails= FXCollections.observableArrayList();
+        ResultSet allOrderDetails = orderDetailRepository.getAllOrderDetails();
         try {
-            Connection connection= DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM OrderDetail;");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
+            while (allOrderDetails.next()){
                 orderDetailManagementDetails.add(new OrderDetailManagementDetails(
-                        resultSet.getString("OrderID"),
-                        resultSet.getString("ItemCode"),
-                        resultSet.getInt("OrderQTY"),
-                        resultSet.getInt("Discount")
+                        allOrderDetails.getString("OrderID"),
+                        allOrderDetails.getString("ItemCode"),
+                        allOrderDetails.getInt("OrderQTY"),
+                        allOrderDetails.getInt("Discount")
                 ));
             }
         } catch (SQLException e) {
