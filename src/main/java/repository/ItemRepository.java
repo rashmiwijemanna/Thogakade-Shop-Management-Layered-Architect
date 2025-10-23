4429c5a480832f283eb1cb26bc89107f27fa93bb
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-10-23 23:22:58 +0530

    create ItemRepository interface in repository package and implemented in ItemRepositoryImpl.java

--- src/main/java/repository/ItemRepository.java
+++ src/main/java/repository/ItemRepository.java
@@ -1,62 +1,11 @@
 package repository;
 
-import db.DBConnection;
+import javafx.collections.ObservableList;
+import model.ItemManagementDetails;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
-public class ItemRepository {
-    public ResultSet getAllItemDetails(){
-        Connection connection= null;
-        try {
-            connection = DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Itemm");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            return resultSet;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public  PreparedStatement addItemDetails(){
-        String SQL="INSERT INTO Itemm(ItemCode, Description, PackSize, UnitPrice, QtyOnHand) VALUES(?,?,?,?,?);";
-
-        try {
-            Connection connection= DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
-            return  preparedStatement;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public PreparedStatement deleteItemDetails(){
-        String SQL="DELETE FROM Itemm WHERE ItemCode = ?";
-        Connection connection = null;
-        try {
-            connection = DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
-            return preparedStatement;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public  PreparedStatement updateItemDetails(){
-        String SQL="UPDATE Itemm SET Description = ?, PackSize = ?, UnitPrice = ?, QtyOnHand = ? WHERE ItemCode = ?";
-        Connection connection= null;
-        try {
-            connection = DBConnection.getInstance().getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
-            return  preparedStatement;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
+public interface ItemRepository {
+     ResultSet getAllItemDetails();
+    public  void  addItemDetails(ItemManagementDetails itemManagementDetails);
 }
